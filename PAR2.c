1da6b4c98429af93890b330f9306e194f78d2467
Om Sharma <omsharma050322@gmail.com>
2022-12-06 23:34:37 +0530

    Add files via upload

---
+++ PAR2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T,N;
+	scanf("%d",&T);
+	while(T--)
+	{
+	    scanf("%d",&N);
+	    if(N % 2 == 0)
+	    {
+	        printf("Yes");
+	        printf("\n");
+	    }
+	    else
+	   {
+	        printf("No");
+	        printf("\n");
+	   }
+	}
+	
+	
+	
+}
+
